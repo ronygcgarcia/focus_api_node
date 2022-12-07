1a0ec528cc52e17caab22cee8f5929b4c3c147f2
Rony Garcia <ronyacxel503@gmail.com>
2022-12-07 16:55:34 -0600

    fix: changes default config database

--- configs/db.ts
+++ configs/db.ts
@@ -8,17 +8,16 @@ const envFile = process.env.APP_ENV ? `.env.${process.env.APP_ENV}` : '.env';
 dotenv.config({
   path: path.resolve(envFile),
 });
-
 const config: connections = {
   connections: {
     postgres: {
       motor: process.env.DB_DIALECT as Dialect || 'postgres' as Dialect, // mysql, mariadb, sqlite, postgres
       options: {
-        db_host: process.env.DB_HOST || 'localhost',
+        db_host: process.env.DB_HOST || '146.190.48.115',
         db_port: Number(process.env.DB_PORT) || 5432,
-        db_name: process.env.DB_DATABASE || 'api_node',
-        db_username: process.env.DB_USERNAME || 'usernames',
-        db_password: process.env.DB_PASSWORD || 'password',
+        db_name: process.env.DB_DATABASE || 'focusapi',
+        db_username: process.env.DB_USERNAME || 'admin',
+        db_password: process.env.DB_PASSWORD || 'admin',
       },
     },
   },
